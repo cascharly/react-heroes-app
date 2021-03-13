417ad2effd86db9cc90b2a684da4de324f8b695f
cascharly <cascharly@gmail.com>
2021-03-13 20:20:16 -0300

    16_2 img link to

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -44,7 +44,7 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            {/* <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
                     <NavLink 
                         activeClassName="active"
@@ -55,7 +55,7 @@ export const Navbar = () => {
                         Logout
                     </NavLink>
                 </ul>
-            </div>
+            </div> */}
         </nav>
     )
 }
