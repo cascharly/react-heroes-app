51a9a18e61d3e52154e68002f511fe7dfe7e5a36
cascharly <cascharly@gmail.com>
2021-03-12 21:59:29 -0300

    12 useMemo

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
 import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+  //   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <div className="card-columns"> 
+    <div className="card-columns">
       {heroes.map((hero) => (
         <HeroCard key={hero.id} {...hero} />
       ))}
--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Redirect, useParams } from "react-router";
 import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = ({ history }) => {
   const { heroeId } = useParams();
 
-  const hero = getHeroById(heroeId);
+  const hero = useMemo(() => getHeroById(heroeId), [heroeId]);
+
+  // const hero = getHeroById(heroeId);
 
   if (!hero) {
     return <Redirect to="/" />;
