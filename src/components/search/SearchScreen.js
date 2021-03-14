0df8cb1a2132e5194016d927dbc3ce945e112385
cascharly <cascharly@gmail.com>
2021-03-14 11:25:34 -0300

    Navbar Collapse

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,94 +1,70 @@
-import React, { useMemo } from 'react';
-import queryString from 'query-string';
-import { HeroCard } from '../heroes/HeroCard';
-import { useForm } from '../../hooks/useForm';
-import { useLocation } from 'react-router-dom';
-import { getHeroesByName } from '../../selectors/getHeroesByName';
+import React, { useMemo } from "react";
+import queryString from "query-string";
+import { HeroCard } from "../heroes/HeroCard";
+import { useForm } from "../../hooks/useForm";
+import { useLocation } from "react-router-dom";
+import { getHeroesByName } from "../../selectors/getHeroesByName";
 
 export const SearchScreen = ({ history }) => {
+  const location = useLocation();
+  const { q = "" } = queryString.parse(location.search);
 
-    const location = useLocation();
-    const { q = '' } = queryString.parse( location.search );
+  const [formValues, handleInputChange] = useForm({
+    searchText: q,
+  });
+  const { searchText } = formValues;
 
-    const [ formValues, handleInputChange ] = useForm({
-        searchText: q
-    });
-    const { searchText } = formValues;
-    
-    // const heroesFiltered = useMemo(() => getHeroesByName( q ), [q])
-    const heroesFiltered = getHeroesByName(searchText)
+  // const heroesFiltered = useMemo(() => getHeroesByName( q ), [q])
+  const heroesFiltered = getHeroesByName(searchText);
 
-    const handleSearch = (e) => {
-        e.preventDefault();
-        history.push(`?q=${ searchText }`);
-    }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    history.push(`?q=${searchText}`);
+  };
 
-    return (
-        <div>
-            <h1>Search Screen</h1>
-            <hr />
-            
-            <div className="row">
-                
-                <div className="col-sm col-md-4">
-                    <h4> Search Form </h4>
-                    <hr />
+  return (
+    <div>
+      <div className="row">
+        <div className="col-sm col-md-4">
+          <h4> Search Form </h4>
 
-                    <form onSubmit={ handleSearch }>
-                        <input 
-                            type="text"
-                            placeholder="Find your hero"
-                            className="form-control"
-                            name="searchText"
-                            autoComplete="off"
-                            value={ searchText }
-                            onChange={ handleInputChange }
-                        />
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Find your hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
 
-                        <button
-                            type="submit"
-                            className="btn mt-1 btn-block btn-outline-primary"
-                        >
-                            Search...
-                        </button>
-                    </form>
-
-                </div>
-
-                <div className="col-sm col-md-8">
-
-                    <h4> Results </h4>
-                    <hr />
-
-                    { 
-                        (q ==='') 
-                            && 
-                            <div className="alert alert-info">
-                                Search a hero
-                            </div>
-                    }
-
-                    { 
-                        (q !=='' && heroesFiltered.length === 0 ) 
-                            && 
-                            <div className="alert alert-danger">
-                                There is no a hero with <strong>{ q }</strong>
-                            </div>
-                    }
-
-                    {
-                        heroesFiltered.map( hero => (
-                            <HeroCard 
-                                key={ hero.id }
-                                { ...hero }
-                            />
-                        ))
-                    }
-
-                </div>
-
-            </div>
-
+            <button
+              type="submit"
+              className="btn mt-1 btn-block btn-outline-primary"
+            >
+              Search...
+            </button>
+          </form>
         </div>
-    )
-}
+
+        <div className="col-sm col-md-8">
+          <h4> Results </h4>
+          <hr />
+
+          {q === "" && <div className="alert alert-info">Search a hero</div>}
+
+          {q !== "" && heroesFiltered.length === 0 && (
+            <div className="alert alert-danger">
+              There is no a hero with <strong>{q}</strong>
+            </div>
+          )}
+
+          {heroesFiltered.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,61 +1,59 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Heroes
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <NavLink
+              activeClassName="active"
+              className="nav-item nav-link"
+              exact
+              to="/marvel"
             >
-                Comics
-            </Link>
+              Marvel
+            </NavLink>
+          </li>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
+          <li className="nav-item">
+            <NavLink
+              activeClassName="active"
+              className="nav-item nav-link"
+              exact
+              to="/dc"
+            >
+              DC
+            </NavLink>
+          </li>
 
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/marvel"
-                    >
-                        Marvel
-                    </NavLink>
-
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
-                    
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/search"
-                    >
-                        Search
-                    </NavLink>
-                </div>
-            </div>
-
-            {/* <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
-                    >
-                        Logout
-                    </NavLink>
-                </ul>
-            </div> */}
-        </nav>
-    )
-}
+          <li className="nav-item">
+            <NavLink
+              activeClassName="active"
+              className="nav-item nav-link"
+              exact
+              to="/search"
+            >
+              Search
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
